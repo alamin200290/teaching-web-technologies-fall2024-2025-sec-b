05fe5836c7274eb8b8a0672f964e20fa714362dd
Md.Al-Amin <amin.se.aiub@gmail.com>
2025-05-15 11:47:27 +0600

    Add files via upload

---
+++ user-mgt/controller/loginCheck.php
@@ -0,0 +1,25 @@
+<?php
+    error_reporting(E_ALL)
+    require_once('../model/userModel.php');
+    session_start();
+    if(isset($_POST['submit'])){
+        $username = trim($_POST['username']);
+        $password = trim($_POST['password']);
+
+        if($username == "" || $password == ""){
+            echo "null username/password!";
+        }else{ 
+            $user = ['username'=> $username, 'password'=>$password];
+            $status = login($user);
+            if($status){
+                setcookie('status', 'true', time()+3000, '/');
+                header('location: ../view/home.php');
+            }else{
+                header('location: ../view/login.html');
+            }
+        }
+    }else{
+        //echo "Invalid request! Please submit form!";
+        header('location: ../view/login.html');
+    }
+?>
